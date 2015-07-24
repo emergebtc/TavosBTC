b41989694095cd989181e9dca8f620a27a54c11b
JM Erestain <smallstratos@hotmail.com>
2015-07-24 22:56:22 +0800

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -122,7 +122,7 @@ __attribute__((unused))
 // You may add here other checkpoints using the following format:
 // {<block height>, "<block hash>"},
 const std::initializer_list<CheckpointData> CHECKPOINTS = {
-  //{ 10000, "84b6345731e2702cdaadc6ce5e5238c4ca5ecf48e3447136b2ed829b8a95f3ad" },
+  { 6264, "c0cc66658d76fc9e9dbd7f55d886360b2ef579e73e1445270d37254f0a8e2e71" },
 };
 
 } // cryptonote
